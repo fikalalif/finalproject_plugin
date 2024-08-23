d225df21ba949d23164d74ac53098a2142909315
fikalalif <fikalgans26@email.com>
2024-08-23 23:31:23 +0700

    final_project

---
+++ app/src/main/java/com/example/finalproject_plugin/slide2/search.kt
@@ -0,0 +1,19 @@
+package com.example.finalproject_plugin.slide2
+
+import androidx.annotation.OptIn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+
+//@Composable
+//fun SearchBar() {
+//    var query by remember {
+//        mutableStateOf("")
+//    var active by remember {
+//        mutableStateOf(false)
+//    }
+//}
